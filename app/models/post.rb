0d59ee6358f52dd402f257d7a46f32314784280d
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-27 17:09:55 -0300

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,18 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_posts_counter
+
+  private
+
   def update_posts_counter
     author.posts_counter = author.posts.count
     author.save
   end
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+  
 end
