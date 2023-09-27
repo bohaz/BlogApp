85a5bd2354807c0000b03b796d142f20c0ca57e3
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-27 17:07:46 -0300

    Move method to private

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :post
 
+private
+
   def update_likes_counter
     post.likes_counter = post.likes.count
     post.save
