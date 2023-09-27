cff96cbc0642c8ec93ca639e036dc04a62925393
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-27 13:20:02 -0300

    Add method to update likes counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_likes_counter
+    self.post.likes_counter = self.post.likes.count
+    self.post.save
+  end
 end
