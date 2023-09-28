9baed4353701a8c4046120a78fd92ec20f46c2d0
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-28 11:56:59 -0300

    Add callback to comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save    :update_comments_counter
+
   private
 
   def update_comments_counter
