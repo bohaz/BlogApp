2be9d182171501ca9d099eba46e0a5dff5145518
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-05 19:49:52 -0300

    Update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params.merge(author: current_user))
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
     if @comment.save
       redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully added.'
     else
-      redirect_to user_post_path(@post.author, @post), alert: 'Error adding comment.'
+      flash[:alert] = @comment.errors.full_messages.to_sentence
+      redirect_to user_post_path(@post.author, @post)
     end
   end
 
