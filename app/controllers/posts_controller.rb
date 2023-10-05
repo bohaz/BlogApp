64b2a6a532d8eaa1b09490b05784e5a0b0bddac7
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-05 18:06:28 -0300

    Add new and create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,30 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @author = User.find(@post.author_id)
   end
 end
+
+def new
+  @post = current_user.posts.build
+end
+
+def create
+  @post = current_user.posts.build(post_params)
+  if @post.save
+    redirect_to user_posts_path(current_user), notice: 'Post was successfully created.'
+  else
+    render :new
+  end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :text)
+end
+
+  
+  
+
+
