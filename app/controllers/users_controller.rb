c5c0287c593724c538602970d5fccc86f3a0aead
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-11 18:55:02 -0300

    Add logic to sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    if params[:id] == "sign_out"
+      sign_out current_user
+      redirect_to root_path and return
+    end
+    
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:comments)
   end
