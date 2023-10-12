990390aac9408a353e2e5855feb0b0ef726ecb46
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-12 13:23:08 -0300

    Add ability.rb file

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # in case of guest user
+
+    if user.role == "admin"
+      can :manage, :all
+    else
+      can :destroy, Post, author_id: user.id
+      can :destroy, Comment, author_id: user.id
+    end
+  end
+end
