c9c3aa173abd2bbee21687a17777234fa5e187d8
over-geek <kenagy100@gmail.com>
2023-10-12 15:16:30 +0000

    Add authorization method to delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,19 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
-
   def show
     @post = Post.find(params[:id])
+    @user = User.find(@post.author_id)
     @author = User.find(@post.author_id)
     @new_post = current_user.posts.build if user_signed_in?
   end
-
   def new
     @user = User.find(params[:user_id])
     @post = @user.posts.build
   end
-
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
@@ -25,9 +22,15 @@ class PostsController < ApplicationController
       render :new
     end
   end
-
+  def destroy
+    @post = Post.find(params[:id])
+    authorize! :destroy, @post
+    @post.comments.destroy_all
+    @post.likes.destroy_all
+    @post.destroy
+    redirect_to user_posts_path(current_user), notice: 'Post was successfully deleted.'
+  end
   private
-
   def post_params
     params.require(:post).permit(:title, :text)
   end
